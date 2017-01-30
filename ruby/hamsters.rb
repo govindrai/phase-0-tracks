b37159b35706b1143d267df44d25886e33afca7b
Govind Rai <raigovind93@gmail.com>
2017-01-29 20:00:44 -0800

    Convert input and print to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,14 +1,14 @@
 puts "What is your hamster's name?"
-name = gets.chomp
+name = gets.chomp.capitalize
 
 puts "Tell me #{name}'s noise level (1-10)"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What is #{name}'s' fur color?"
 fur_color = gets.chomp
 
-puts "Is #{name} a good candidate for adoption? (Y/N)"
-canditate_type = gets.chomp
+puts "#{name} a good candidate for adoption? (true/false)"
+candidate_type = gets.chomp
 
 puts "Approximately how old is #{name}"
 age = gets.chomp
@@ -18,7 +18,7 @@ if age == ''
 end
 
 # puts(name,volume,fur_color,canditate_type,age)
-
+puts("Name: #{name}\nNoise Level: #{volume}\nAge: #{age}\nFur Color: #{fur_color}\nAdoptable?: #{candidate_type}")
 
 
 
