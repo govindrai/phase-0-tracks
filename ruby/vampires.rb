ce3f590ce8b1914ce4a701ef0d1445c941d5fbf7
Govind Rai <raigovind93@gmail.com>
2017-01-30 00:05:35 -0800

    Add vampire detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,36 @@ garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance? (Y/N)"
 enroll = gets.chomp
+
+
+def checkYear(age,year)
+    puts "Year is: #{year}"
+    puts("Year should be: #{2017-age}")
+    return (2017-age) == year
+end
+
+correct_age = checkYear(age,year)
+garlic = garlic == 'Y'
+enroll = enroll == 'Y'
+
+if correct_age && (garlic || enroll)
+    result = "Probably not a vampire"
+end
+
+if !correct_age && (!garlic || !enroll)
+    result = "Probably a vampire"
+end
+
+if !correct_age && !garlic && !enroll
+    result = "Almost certainly a vampire"
+end
+
+if name == 'Drake Cula' || name == 'Tu Fang'
+    result = 'Definitely a vampire'
+end
+
+if result
+    puts result
+else
+    puts "Results inconclusive"
+end
