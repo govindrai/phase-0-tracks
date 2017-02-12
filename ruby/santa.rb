8c4e3ced1db7b6a18dabad0f6c502b5d2191ada1
Govind Rai <raigovind93@gmail.com>
2017-02-12 13:53:36 -0800

    Add initializaiton variables

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,10 @@
 class Santa
-    def initialize
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+    def initialize(gender, ethnicity)
+        @gender = gender
+        @ethnicity = ethnicity
+        @reindeer_ranking
         puts "Initializing Santa instance"
     end
 
@@ -15,3 +20,10 @@ end
 
 cally = Santa.new
 cally.speak
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
